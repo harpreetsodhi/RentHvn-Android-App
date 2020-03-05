7e17ba8a2933108981a4ba37a4153e3701e89a03
Haard Prashant Shah <hr606438@dal.ca>
2020-03-05 14:58:45 -0400

    Added bottom navigation_bar and home_screen skeleton

--- app/src/main/java/com/example/renthvn/MainActivity.kt
+++ app/src/main/java/com/example/renthvn/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.renthvn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, Home_Screen::class.java)
+        // start your next activity
+        startActivity(intent)
     }
 }
