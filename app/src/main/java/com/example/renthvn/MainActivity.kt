708544f338583302d7715283d96d7f8ca9eadc5f
Harpreet Singh <hr557871@dal.ca>
2020-03-09 01:05:38 -0300

    Cart page implemented

--- app/src/main/java/com/example/renthvn/MainActivity.kt
+++ app/src/main/java/com/example/renthvn/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.renthvn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        btn.setOnClickListener {
+//            val intent = Intent(this, Cart::class.java)
+//            startActivity(intent)
+//        }
     }
+
+
 }
