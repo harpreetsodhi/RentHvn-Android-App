99b3969cbe132b7fd612eeebefd4cf05ea77abe9
Tejasvi Vig <tejasvivig09@gmail.com>
2020-03-30 12:01:40 -0300

    Donation Activity

--- app/src/main/java/com/example/renthvn/DonationActivity.kt
+++ app/src/main/java/com/example/renthvn/DonationActivity.kt
@@ -4,11 +4,18 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_donation2.*
 
 class DonationActivity : AppCompatActivity() {
     var image_rui: Uri? = null
+
+    private var mDatabase: FirebaseDatabase? = null
+
+    private var mDatabaseReference: DatabaseReference? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,31 +34,56 @@ class DonationActivity : AppCompatActivity() {
 
     private fun saveData(){
 
-
-
-
-
         val name = intent.getStringExtra("nameOfDonor")
         val email = intent.getStringExtra("emailOfDonor")
         val phoneNumber = intent.getStringExtra("phoneNumberOfDonor")
         val description = intent.getStringExtra("description")
 
-       // val db = FirebaseDatabase.getInstance()
 
-        val ref = FirebaseDatabase.getInstance().getReference("users")
-        val userId = ref.push().key
+        Toast.makeText(this,"Thanks for the donation",Toast.LENGTH_SHORT).show()
 
+        val myDatabase = FirebaseDatabase.getInstance().getReference("users")
+        val userId = myDatabase.push().key
         val user = DonationUser(userId,name,email,phoneNumber,description)
-
-        ref.child(userId.toString()).setValue(user).addOnCompleteListener{
-            Toast.makeText(this,"Value has been entered in the database",Toast.LENGTH_SHORT).show()
+        myDatabase.child(userId.toString()).setValue(user).addOnCompleteListener(){
+            Toast.makeText(this,"Thanks for the donation",Toast.LENGTH_SHORT).show()
         }
 
-//        val user = DonationUser(name,email,phoneNumber,description)
 
-  //      val userId = ref.child("users").push().key
 
-    //    ref.child("users").setValue(user)
+       // val fireDatabase = FirebaseDatabase.getInstance()
+
+        //val databaseReference = fireDatabase.getReference("renthvn-995ea")
+
+        //val user = DonationUser(name,email,phoneNumber,description)
+        //val key = databaseReference.child("users").push().key
+        //databaseReference.child("users").child(key.toString()).setValue(user)
+
+
+
+        //val userid: String?
+
+        //userid = ref.push().key
+
+
+        //ref.child(userid.toString()).setValue("tejasvi")
+
+
+
+        //val ref = FirebaseDatabase.getInstance().getReference("")
+        //val userId = ref.push().key
+        // currentDonationUser = ref!!.child(userId.toString())
+
+      //  val user = DonationUser(name,email,phoneNumber,description)
+
+      //  currentDonationUser.setValue(user).addOnCompleteListener(){
+     //       Toast.makeText(this,"Value has been entered in the database",Toast.LENGTH_SHORT).show()
+    //    }
+
+      //ref.child(userId.toString()).setValue(user).addOnCompleteListener{
+       //Toast.makeText(this,"Value has been entered in the database",Toast.LENGTH_SHORT).show()
+      //}
+
 
     }
 }
--- app/src/main/java/com/example/renthvn/DonationUser.kt
+++ app/src/main/java/com/example/renthvn/DonationUser.kt
@@ -1,4 +1,6 @@
 package com.example.renthvn
 
 
-class DonationUser(val userId: String?, val name: String? , val email: String? , val phoneNumber: String? , val description: String?)
+class DonationUser(val userId: String?, val name: String? , val email: String? , val phoneNumber: String? , val description: String?){
+    constructor():this( "","","","","")
+}
