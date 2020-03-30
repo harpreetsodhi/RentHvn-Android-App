09c8fe3deeca7ca8e1c8822e69e998d5fa6e8e90
Aditya Patel <ad723057@dal.ca>
2020-03-30 03:06:15 -0300

    Add email verification and validation

--- app/src/androidTest/java/com/project/renthvn/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/project/renthvn/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.renthvn
+package com.project.renthvn
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.renthvn", appContext.packageName)
+        assertEquals("com.project.renthvn", appContext.packageName)
     }
 }
